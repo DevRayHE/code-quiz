e82ba18d1b82e47969f77f644551d72c083c5a0c
Ray He <devrayhe@gmail.com>
2021-09-10 23:59:56 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# code-quiz
+# 💻 Project title
+
+## Code Quiz
+
+<br/>
+
+# 📓 Description
+
+###
+
+<br/>
+
+# 🎇 Features
+
+- 
+
+- 
+
+-
+
+-
+
+-
+
+-
+
+<br/>
+
+# ️🖼️Screenshot
+
+## The following image shows the web application's appearance and functionality:
+
+<br/>
+
+### Sample website intro video.
+
+![Sample gif video.](./assets/image/ "video")
+
+<br/>
+
+# 🏗️ Installation
+
+The link to the deployed site:
+
+[Deployed site](https://devrayhe.github.io/code-quiz/)
+
+The link to the repo url:
+
+[Repo url](https://github.com/DevRayHE/code-quiz.git)
---
+++ assets/js/script.js
@@ -0,0 +1,99 @@
+var questionEl = document.getElementById("question");
+// document.querySelector("#question")
+var answer1El = document.getElementById("answer1");
+var answer2El = document.getElementById("answer2")
+var answer3El = document.getElementById("answer3")
+var answer4El = document.getElementById("answer4")
+
+
+// Stores questions in an array of objects
+var questions = [
+    {
+        question: "question 1?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: 0,
+    },
+    {
+        question: "question 2?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: 0,
+    },
+    {
+        question: "question 3?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: 0,
+    },
+    {
+        question: "question 4?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: 0,
+    },
+    {
+        question: "question 5?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: 0,
+    },
+    {
+        question: "question 6?",
+        answers: ["a", "b", "c", "d"],
+        correctAnswer: 0,
+    },
+];
+
+var currentQuestionIndex = 0;
+
+function updateQuestion () {
+    var currentQuestion = questions[currentQuestionIndex];
+
+    questionEl.innerText = currentQuestion.question;
+    answer1El.innerText = currentQuestion.answers[0];
+    answer2El.innerText = currentQuestion.answers[1];
+    answer3El.innerText = currentQuestion.answers[2];
+    answer4El.innerText = currentQuestion.answers[3];
+
+    // currentQuestionIndex++;
+}
+
+function checkAnswer (clickedAnswer) {
+    var currentQuestion = questions[currentQuestionIndex];
+
+    if (currentQuestion === null) {
+        return;
+    }
+
+    if (currentQuestion.correctAnswer === clickedAnswer) {
+        console.log("correct answer");
+    }
+    else {
+        console.log("wrong answer");
+    }
+
+    currentQuestionIndex++;
+    updateQuestion ();
+}
+
+answer1El.addEventListener("click", function () {
+    checkAnswer(0);
+});
+answer2El.addEventListener("click", function () {
+    checkAnswer(1);
+});
+answer3El.addEventListener("click", function () {
+    checkAnswer(2);
+});
+answer4El.addEventListener("click", function () {
+    checkAnswer(3);
+});
+
+
+updateQuestion();
+
+
+// Seperate codes into different functions:
+
+// init function to call when start quiz is clicked
+// timer function to control time
+// check right/wrong selection function
+// render function to render elements based on user input
+// function to setItem and or getItem from localStorage
+// 
